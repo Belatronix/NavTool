3bc0b9ddb1a2bb96c07b8d7ac42aaa533e46d741
abeierlein <abetronix@gmail.com>
2015-03-01 23:15:38 +0100

    --some little changes

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import sys
 import platform
 import PySide
+import numpy as np
+
 from PySide import QtGui
-from PySide.QtGui import QApplication, QMainWindow, QMessageBox, QDialog
+
+from PySide.QtGui import QApplication, QMainWindow, QMessageBox
+
 from navtool_ui import Ui_MainWindow
 from dialog_options_ui import Ui_Dialog
 
-__version__ = '0.0.1'
+
+__version__ = '0.0.2'
+
 
 class MyApplication(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
@@ -14,7 +20,7 @@ class MyApplication(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.action_ber_das_NavTool.triggered.connect(self.about)
         self.action_ffnen.triggered.connect(self.openfile)
-        self.action_ber_den_Autor.triggered.connect(lambda: self.opendialog("Was ist denn hier los?"))
+        self.action_ber_den_Autor.triggered.connect(lambda: self.opendialog("Wos ist denn hier los?"))
         self.action_ber_QT.triggered.connect(self.about_qt)
         self.actionHilfe_zu_den_Tools.triggered.connect(self.text)
 
@@ -27,17 +33,21 @@ class MyApplication(QMainWindow, Ui_MainWindow):
                <p align="center">This application can be used for
                displaying OS and platform details.
                <p align="center">Python %s -  PySide version %s - Qt version %s on %s""" % \
-                (__version__, platform.python_version(), PySide.__version__,\
-                 PySide.QtCore.__version__, platform.system()))
+                          (__version__, platform.python_version(), PySide.__version__, PySide.QtCore.__version__,
+                           platform.system()))
 
     def about_qt(self):
         QMessageBox.aboutQt(self)
 
     def text(self):
-        QMessageBox.warning(self, "Warnung", "Hups, da ist jetzt was schief gegangen!")
+        QMessageBox.warning(self, "Warnung", "Ojojoy, da ist jetzt was schief gegangen!")
 
     def openfile(self):
-        fname, _ = QtGui.QFileDialog.getOpenFileName(self, 'Log File oeffnen',  '/home')
+        try:
+            fname, _ = QtGui.QFileDialog.getOpenFileName(self, 'Log File oeffnen', '/home')
+            d = np.genfromtxt(fname, names=True, delimiter=";")
+        except ValueError as errrortext:
+            QMessageBox.warning(self, errrortext)
 
     def opendialog(self, text=""):
         dialog = QtGui.QDialog(self)
@@ -48,8 +58,9 @@ class MyApplication(QMainWindow, Ui_MainWindow):
         dialogUI.buttonBox.button(QtGui.QDialogButtonBox.Cancel).clicked.connect(dialog.reject)
         dialogUI.buttonBox.button(QtGui.QDialogButtonBox.Ok).clicked.connect(self.about)
 
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MyApplication()
     window.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec_()) #exec_ instead of 'exec' cause this is an keyword in Python
