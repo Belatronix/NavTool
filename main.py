a2a39461a1e123af6155dad78c10b0f414cbc045
abeierlein <abetronix@gmail.com>
2015-03-02 10:36:01 +0100

    --correct typos

--- main.py
+++ main.py
@@ -20,47 +20,46 @@ class MyApplication(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.action_ber_das_NavTool.triggered.connect(self.about)
         self.action_ffnen.triggered.connect(self.openfile)
-        self.action_ber_den_Autor.triggered.connect(lambda: self.opendialog("Wos ist denn hier los?"))
+        self.action_ber_den_Autor.triggered.connect(lambda: self.opendialog("What's up here?"))
         self.action_ber_QT.triggered.connect(self.about_qt)
         self.actionHilfe_zu_den_Tools.triggered.connect(self.text)
-
-    def about(self):
-        QMessageBox.about(self, "Ueber das Programm",
-                          """<b>About this Program Version %s</b>
-                <p align="center">Copyright 2014 Axel Beierlein.
-               <p align="center">All rights reserved in accordance with
-               GPL v2 or later - NO WARRANTIES!
-               <p align="center">This application can be used for
-               displaying OS and platform details.
-               <p align="center">Python %s -  PySide version %s - Qt version %s on %s""" % \
-                          (__version__, platform.python_version(), PySide.__version__, PySide.QtCore.__version__,
-                           platform.system()))
 
     def about_qt(self):
         QMessageBox.aboutQt(self)
 
+    def about(self):
+        QMessageBox.about(self, "About the Program",
+                          ('<b>About this Program Version %s</b>\n'
+                           '            <p align="center">Copyright 2015 Axel Beierlein.\n'
+                           '            <p align="center">All rights reserved in accordance with\n'
+                           '            GPL v2 or later - NO WARRANTIES!\n'
+                           '            <p align="center">This application can be used for\n'
+                           '            displaying OS and platform details.\n'
+                           '            <p align="center">Python %s -  PySide version %s - Qt version %s on %s') % (
+                              __version__, platform.python_version(), PySide.__version__, PySide.QtCore.__version__,
+                              platform.system()))
+
     def text(self):
-        QMessageBox.warning(self, "Warnung", "Ojojoy, da ist jetzt was schief gegangen!")
+        QMessageBox.warning(self, "Waning", "Oh no!\nThere is something going wrong!")
 
     def openfile(self):
         try:
-            fname, _ = QtGui.QFileDialog.getOpenFileName(self, 'Log File oeffnen', '/home')
-            d = np.genfromtxt(fname, names=True, delimiter=";")
-        except ValueError as errrortext:
-            QMessageBox.warning(self, errrortext)
+            fname, _ = QtGui.QFileDialog.getOpenFileName(self, 'Open Log File', '/home')
+        except IOError:
+            QMessageBox.warning(self, "Error" "While opening the File")
 
     def opendialog(self, text=""):
         dialog = QtGui.QDialog(self)
-        dialogUI = Ui_Dialog()
-        dialogUI.setupUi(dialog)
+        dialogui = Ui_Dialog()
+        dialogui.setupUi(dialog)
         dialog.show()
-        dialogUI.label.setText(text)
-        dialogUI.buttonBox.button(QtGui.QDialogButtonBox.Cancel).clicked.connect(dialog.reject)
-        dialogUI.buttonBox.button(QtGui.QDialogButtonBox.Ok).clicked.connect(self.about)
+        dialogui.label.setText(text)
+        dialogui.buttonBox.button(QtGui.QDialogButtonBox.Cancel).clicked.connect(dialog.reject)
+        dialogui.buttonBox.button(QtGui.QDialogButtonBox.Ok).clicked.connect(self.about)
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MyApplication()
     window.show()
-    sys.exit(app.exec_()) #exec_ instead of 'exec' cause this is an keyword in Python
+    sys.exit(app.exec_())  # exec_ instead of 'exec' cause this is an keyword in Python
